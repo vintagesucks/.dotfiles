a5c91c678832593badb7bc5150863d3d1bfc04f6
Nikolas Evers <vintagesucks@users.noreply.github.com>
2024-02-20 14:14:56 +0100

    composer 2.7.0

--- .zsh/composer.sh
+++ .zsh/composer.sh
@@ -1,17 +1,10 @@
 function composer-bump {
-    valet composer require "$1" -W
+    valet composer update "$1" -Wm
     valet composer bump
     valet composer validate || true
 }
 
 function composer-set {
-    valet composer require "$1"
-    valet composer require $(echo $1 | cut -d: -f1):^$(echo $1 | cut -d: -f2) --no-update
-    valet composer update --lock
-    valet composer validate || true
-}
-
-function composer-set-w {
     valet composer require "$1"
     valet composer require $(echo $1 | cut -d: -f1):^$(echo $1 | cut -d: -f2) --no-update
     valet composer update --lock
@@ -41,11 +34,4 @@ function composer-set-indirect-phar {
     COMPOSER_MEMORY_LIMIT=-1 valet php ./composer.phar remove $(echo $1 | cut -d: -f1) --no-update
     COMPOSER_MEMORY_LIMIT=-1 valet php ./composer.phar update --lock
     COMPOSER_MEMORY_LIMIT=-1 valet php ./composer.phar validate || true
-}
-
-function composer-bump-indirect {
-    valet composer require "$1"
-    valet composer remove $(echo $1 | cut -d: -f1) --no-update
-    valet composer update --lock
-    valet composer validate || true
 }
