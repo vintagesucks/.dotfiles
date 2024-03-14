0a8b966e2b51b108846f0be9d3fa875cf150c43b
Nikolas Evers <vintagesucks@users.noreply.github.com>
2024-03-14 18:31:48 +0100

    add `release`

--- .zsh/git.sh
+++ .zsh/git.sh
@@ -1,3 +1,19 @@
 function shortlog {
     git shortlog --summary --numbered --email
 }
+
+function release {
+    if ! $(git config --get push.followTags); then
+        echo 'Please enable push.followTags in the current repository:';
+        echo '';
+        echo '  git config push.followTags true';
+
+        return 1;
+    fi
+
+    git checkout -b release/$1 develop
+    git checkout master
+    git merge --no-ff release/$1
+    git tag -a -m '' $1
+    git branch -d release/$1
+}
